6ce8745046a67c08841697d1e8ec7f4f85a0a690
Michael Escamilla <MichaelEscamilla@protonmail.com>
2025-03-05 12:22:58 -0800

    Update Comment-based Help for Private Select Functions

--- private/Select/Select-OSDWorkspaceBootMedia.ps1
+++ private/Select/Select-OSDWorkspaceBootMedia.ps1
@@ -1,4 +1,36 @@
 function Select-OSDWorkspaceBootMedia {
+    <#
+    .SYNOPSIS
+        Selects an OSDWorkspace BootMedia.
+
+    .DESCRIPTION
+        This function displays available OSDWorkspace BootMedia in an Out-GridView and returns the selected BootMedia object.
+        Utilizes the Get-OSDWorkspaceBootMedia function to retrieve the BootMedia.
+
+    .PARAMETER Architecture
+        Filter BootMedia by architecture.
+
+    .INPUTS
+        None.
+
+        You cannot pipe input to this cmdlet.
+
+    .OUTPUTS
+        PSObject
+
+        This function returns the selected BootMedia object.
+
+    .EXAMPLE
+        Select-OSDWorkspaceBootMedia
+        Will display all available BootMedia and return the selected BootMedia in a PSObject.
+
+    .EXAMPLE
+        Select-OSDWorkspaceBootMedia -Architecture 'amd64'
+        Will display all available BootMedia for the architecture 'amd64' and return the selected BootMedia object.
+
+    .NOTES
+        David Segura
+    #>
     [CmdletBinding()]
     param (
         [ValidateSet('amd64', 'arm64')]
--- private/Select/Select-OSDWorkspaceGitHubRepo.ps1
+++ private/Select/Select-OSDWorkspaceGitHubRepo.ps1
@@ -1,4 +1,33 @@
 function Select-OSDWorkspaceGitHubRepo {
+       <#
+    .SYNOPSIS
+        Selects an OSDWorkspace Library GitHub Reposiotry.
+
+    .DESCRIPTION
+        This function displays available OSDWorkspace Library GitHub Repositories in an Out-GridView and returns the selected BootMedia object.
+        Utilizes the Get-OSDWorkspaceGitHubRepo function to retrieve the available HitHub Repositories.
+
+    .INPUTS
+        None.
+
+        You cannot pipe input to this cmdlet.
+
+    .OUTPUTS
+        PSObject
+
+        This function returns the selected GitHub Repository in a PSobject.
+
+    .EXAMPLE
+        Select-OSDWorkspaceBootMedia
+        Will display all available BootMedia and return the selected BootMedia in a PSObject.
+
+    .EXAMPLE
+        Select-OSDWorkspaceBootMedia -Architecture 'amd64'
+        Will display all available BootMedia for the architecture 'amd64' and return the selected BootMedia object.
+
+    .NOTES
+        David Segura
+    #>
     [CmdletBinding()]
     [OutputType([System.IO.FileSystemInfo])]
     param ()
